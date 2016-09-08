ce256126b572b593c796491fc389a36a3adf55d0
Casey Olson <casey@bookcasey.com>
2016-09-07 20:49:23 -0500

    working skeleton

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use('/', express.static('build'));
 
+app.get('/api/hello', function(req, res) {
+  res.json({message: 'Hello from express!'})
+});
+
 app.listen(3000, function () {
   console.log('Listening at 3000!');
 });
